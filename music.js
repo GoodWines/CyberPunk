9b3e72a8600d3c35b73141a981fbe66a230b313e
GoodWines <slaventius2005@gmail.com>
2026-01-17 00:58:20 +0200

    music

--- music.js
+++ music.js
@@ -1,9 +1,11 @@
+// ===================== MAIN ТРЕК =====================
+
 const btn = document.getElementById("musicToggle");
 const music = document.getElementById("music");
 const volume = document.getElementById("volume");
 
 // стартова гучність
-music.volume = volume.value;
+music.volume = 0.5 * volume.value;
 
 btn.addEventListener("click", () => {
   if (music.paused) {
@@ -17,5 +19,36 @@ btn.addEventListener("click", () => {
 
 // керування гучністю
 volume.addEventListener("input", () => {
-  music.volume = volume.value;
+  music.volume = 0.5 * volume.value;
 });
+
+
+// ===================== ІНШІ ТРЕКИ =====================
+const otherTracks = document.querySelectorAll(".cyber-music audio");
+
+otherTracks.forEach(track => {
+  // стартова гучність
+  track.volume = 0.5 * volume.value;
+
+  track.addEventListener("play", () => {
+    stopAllExcept(track); // зупиняємо всі інші треки
+  });
+});
+
+
+// ФУНКЦІЯ ЗУПИНКИ ВСІХ
+function stopAllExcept(currentTrack) {
+  // зупиняємо кастомний трек
+  if (music !== currentTrack) {
+    music.pause();
+    btn.classList.remove("playing");
+  }
+
+  // зупиняємо інші треки
+  otherTracks.forEach(track => {
+    if (track !== currentTrack) {
+      track.pause();
+      track.currentTime = 0;
+    }
+  });
+}
